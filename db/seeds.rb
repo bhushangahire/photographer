27ca2145446d5b826f6161eab88a98d1bcf08230
Bhushan Ahire <bhushangahire2007@gmail.com>
2012-12-14 17:58:14 +0530

    Modified with my needs.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,23 @@
 puts "Creating photographer"
 photographer = Photographer.create(
-  :email => "photographer@grokphoto.org",
+  :email => "clicks@bhushangahire.net",
   :password => "password",
-  :site_url => 'http://demo.grokphoto.org',
-  :company_name => 'GrokPhoto', 
-  :company_email => 'info@grokphoto.org',
-  :blog_url => 'http://rapin.com',
+  :site_url => 'http://clicks.bhushangahire.net',
+  :company_name => 'Clicks', 
+  :company_email => 'bhushangahire2007@gmail',
+  :blog_url => 'http://clicks.bhushangahire.net',
   :use_watermark => false,
   :theme => 'default',
-  :google_analytics_key => 'UA-2450369-23',
-  :google_verification_key => 'n4L_tj44fmy3dxPuLamOqAkrjRP7GW-ehMzRcN6mjM0',
-  :home_page_title => 'Open-source Photography'
+  :google_analytics_key => 'UA-8004674-3',
+  :google_verification_key => '',
+  :home_page_title => 'Clicks by Bhushan Ahire'
 )
 puts "Uploading watermark..."
 photographer.watermark = File.new(File.join(Rails.root, "photoshop/watermark.png")) rescue nil
 photographer.save
 
 puts "Creating clients"
-client = photographer.clients.create(:name => 'Demo Client', :email => "client@grokphoto.org", :password => "password")
+client = photographer.clients.create(:name => 'Demo Client', :email => "client@bhushangahire.net", :password => "password")
 
 puts "Creating bookings"
 bookings = []
@@ -52,7 +52,7 @@ end
 
 puts "Creating pages"
 pages = []
-pages << photographer.pages.create(:position => 1, :title => 'About', :intro => "So what's it all about?", :keywords => "about, story, ruby, rails, photography, professional, custom, page management, wiki, markdown, opensource", :body => "The Story\n---------\n\nI needed to build a professional photography site for a friend along with a way for her to easily manage clients, bookings, and photos.\n\nWhile there are plenty of open source galleries out there, most are written in PHP whereas my personal preference is Ruby on Rails. Also, I was unable to find any that supported the more advanced features we needed for managing client photos and workflow.\n\nAnd so grokphoto was born.\n\nYou can access the admin area at:\n\n* [http://www.grokphoto.org/admin](http://www.grokphoto.org/admin)\n* Email: photographer@grokphoto.org\n* Password: password\n\nYou can access the client area at:\n\n* [http://www.grokphoto.org/client](http://www.grokphoto.org/client)\n* Email: client@grokphoto.org\n* Password: password")
+pages << photographer.pages.create(:position => 1, :title => 'About', :intro => "So what's it all about?", :keywords => "about, story, ruby, rails, photography, professional, custom, page management, wiki, markdown, opensource", :body => "The Story\n---------\n\nI needed to build a professional photography site for a friend along with a way for her to easily manage clients, bookings, and photos.\n\nWhile there are plenty of open source galleries out there, most are written in PHP whereas my personal preference is Ruby on Rails. Also, I was unable to find any that supported the more advanced features we needed for managing client photos and workflow.\n\nAnd so grokphoto was born.\n\nYou can access the admin area at:\n\n* [http://www.bhushangahire.net/admin](http://www.bhushangahire.net/admin)\n* Email: photographer@bhushangahire.net\n* Password: password\n\nYou can access the client area at:\n\n* [http://www.bhushangahire.net/client](http://www.bhushangahire.net/client)\n* Email: client@bhushangahire.net\n* Password: password")
 pages << photographer.pages.create(:position => 2, :title => 'Opensource', :intro => 'GrokPhoto is free!', :keywords => "opensource, ruby, rails, photography, git, haml, ajax, jquery, themes, erb", :body => "Get the Source\n-----------\n\n[http://github.com/rapind/grokphoto](http://github.com/rapind/grokphoto)\n\nFrom Wikipedia\n------------\n\nOpen-source software (OSS) is computer software for which the source code and certain other rights normally reserved for copyright holders are provided under a software license that meets the Open Source Definition or that is in the public domain. This permits users to use, change, and improve the software, and to redistribute it in modified or unmodified forms. It is very often developed in a public, collaborative manner. Open-source software is the most prominent example of open-source development and often compared to user-generated content. The term open-source software originated as part of a marketing campaign for free software. A report by Standish Group states that adoption of open-source software models has resulted in savings of about $60 billion per year to consumers.\n\n[http://en.wikipedia.org/wiki/Open_source_software](http://en.wikipedia.org/wiki/Open_source_software)")
 
 puts "Uploading page images"
